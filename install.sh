5f8f7212a13ea6a41c218b2c906d9a528ff0c875
Gustavo Franco <gufranco@users.noreply.github.com>
2018-08-01 16:29:04 -0300

    Bug fixes

--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@ case "$(uname)" in
     # Repositories
     #
     # Docker
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    wget -q https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     # Node.js
-    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    wget -q https://deb.nodesource.com/setup_10.x | sudo -E bash -
     # Yarn
-    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    wget -q https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb [arch=amd64] https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     # Spotify
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
     echo "deb [arch=amd64] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
     # Chrome
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    wget -q https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
     echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
     # VirtualBox
     sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
@@ -62,8 +62,6 @@ case "$(uname)" in
       transmission \
       vlc \
       steam \
-      linux-image-extra-"$(uname -r)" \
-      linux-image-extra-virtual \
       apt-transport-https \
       ca-certificates \
       software-properties-common \
@@ -85,26 +83,19 @@ case "$(uname)" in
       shellcheck \
       ruby
 
-    # Tilix
-    mkdir -p ~/.config/tilix/schemes
-    ln -s ~/.dotfiles/themes/Tilix/Dracula.json ~/.config/tilix/schemes/Dracula.json
-
     # Atom packages
     apm install \
       sync-settings
-
-    # Ruby packages
-    sudo gem install \
-      bundler \
-      rails
 
     # Node.js packages
     sudo yarn global add \
       eslint --ignore-optional
 
-    # Nerd fonts
-    git clone https://github.com/ryanoasis/nerd-fonts.git --depth=1 /tmp/fonts
-    ./tmp/fonts/install.sh
+    # Hack Nerd Font
+    mkdir -p ~/.local/share/fonts
+    cd ~/.local/share/fonts || exit 1
+    curl -fLo "Hack Regular Nerd Font Complete.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
+    sudo fc-cache -fv
 
     ;;
   Darwin)
@@ -180,11 +171,6 @@ case "$(uname)" in
     apm install \
       sync-settings
 
-    # Ruby packages
-    gem install \
-      bundler \
-      rails
-
     # Node.js packages
     sudo yarn global add \
       eslint \
@@ -213,21 +199,27 @@ esac
 #
 # Delete old files
 rm -rf ~/.dotfiles ~/.zshrc ~/.gitconfig ~/.vim ~/.vimrc ~/.gnupg ~/.ssh ~/.muttrc ~/.mutt ~/.mailcap ~/.tmux.conf ~/.tmux
+
 # Clone dotfiles
-git clone --recursive https://github.com/gufranco/dotfiles.git ~/.dotfiles --depth=1 && \
-cd ~/.dotfiles || exit 1 && \
+git clone --recursive https://github.com/gufranco/dotfiles.git ~/.dotfiles --depth=1
+cd ~/.dotfiles || exit 1
 git remote set-url origin git@github.com:gufranco/dotfiles.git
+
 # Zsh
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/themes/zsh/dracula.zsh-theme ~/.dotfiles/zsh/.oh-my-zsh/custom/themes/dracula.zsh-theme
 echo "$(which zsh)" | sudo tee /etc/shells
 sudo sed -i -- 's/auth       required   pam_shells.so/# auth       required   pam_shells.so/g' /etc/pam.d/chsh
 sudo chsh $USER -s "$(which zsh)"
+
 # Git
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
+
 # Vim
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
+vim +PlugUpgrade +PlugInstall +qall
+
 # GPG
 ln -s ~/.dotfiles/gnupg ~/.gnupg
 if [[ "$(uname)" == "Linux" ]]; then
@@ -239,16 +231,23 @@ chmod 700 ~/.gnupg
 chmod 400 ~/.gnupg/keys/*
 gpg --import ~/.gnupg/keys/personal.public
 gpg --import ~/.gnupg/keys/personal.private
+
 # SSH
 ln -s ~/.dotfiles/ssh ~/.ssh
 chmod 400 ~/.ssh/id_rsa
+
 # NeoMutt
 ln -s ~/.dotfiles/mutt/.muttrc ~/.muttrc
 ln -s ~/.dotfiles/mutt ~/.mutt
 ln -s ~/.dotfiles/mutt/.mailcap ~/.mailcap
+
 # Tmux
 ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/tmux ~/.tmux
 bash ~/.tmux/plugins/tpm/scripts/install_plugins.sh
-# Finish
-sudo shutdown -r now
+
+# Tilix
+if [[ "$(uname)" == "Linux" ]]; then
+  mkdir -p ~/.config/tilix/schemes
+  ln -s ~/.dotfiles/themes/Tilix/Dracula.json ~/.config/tilix/schemes/Dracula.json
+fi
