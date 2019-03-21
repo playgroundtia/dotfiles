68b3a1a2ee1a8a8b9dc545146643ec79a26b8b8f
Gustavo Franco <gufranco@users.noreply.github.com>
2019-03-20 22:02:20 -0300

    Use docker-compose as binary

--- install.sh
+++ install.sh
@@ -177,6 +177,12 @@ case "$(uname)" in
     sudo usermod -a -G docker "$USER"
 
     ############################################################################
+    # Docker compose
+    ############################################################################
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+
+    ############################################################################
     # Conky
     ############################################################################
     if [ -f ~/.conkyrc ] || [ -h ~/.conkyrc ]; then
