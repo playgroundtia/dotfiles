3e6b608383979802c8268af360b673672a0600d9
Gustavo Franco <gufranco@users.noreply.github.com>
2019-06-04 13:31:33 -0300

    Run docker-compose as container

--- install.sh
+++ install.sh
@@ -83,9 +83,6 @@ case "$(uname)" in
       docker-ce
     sudo usermod -a -G docker "$USER"
 
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-
     ############################################################################
     # Node.js / Yarn
     ############################################################################
