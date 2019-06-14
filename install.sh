e55fb802f62c6409e264ffff42b95ab01a098512
Gustavo Franco <gufranco@users.noreply.github.com>
2019-06-14 15:36:58 -0300

    Add Sublime Text

--- install.sh
+++ install.sh
@@ -236,6 +236,15 @@ case "$(uname)" in
       intellij-idea-community
 
     ############################################################################
+    # Sublime Text 3
+    ############################################################################
+    curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+    echo "deb [arch=amd64] https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+    sudo apt-get update
+    sudo apt-get install -y \
+      sublime-text
+
+    ############################################################################
     # terraform
     ############################################################################
     curl -fsSL https://tjend.github.io/repo_terraform/repo_terraform.key | sudo apt-key add -
@@ -510,6 +519,7 @@ case "$(uname)" in
       soda-player \
       spotify \
       steam \
+      sublime-text \
       transmission \
       vagrant \
       virtualbox \
